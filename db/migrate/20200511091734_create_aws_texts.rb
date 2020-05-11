81bfdd52cef070b6d945eeaa249cb593fa20ae74
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-11 19:24:32 +0900

    型の不要なブランクの削除とマイグレーションのリセット

--- db/migrate/20200511091734_create_aws_texts.rb
+++ db/migrate/20200511091734_create_aws_texts.rb
@@ -2,7 +2,7 @@ class CreateAwsTexts < ActiveRecord::Migration[6.0]
   def change
     create_table :aws_texts do |t|
       t.string :title
-      t.text　:content
+      t.text :content
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_092548) do
 
   create_table "aws_texts", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
