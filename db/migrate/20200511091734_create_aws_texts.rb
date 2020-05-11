67915721b697950191cd2af17f363184af8fd13a
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-11 19:10:05 +0900

    型の修正とマイグレーションのリセット

--- db/migrate/20200511091734_create_aws_texts.rb
+++ db/migrate/20200511091734_create_aws_texts.rb
@@ -2,7 +2,7 @@ class CreateAwsTexts < ActiveRecord::Migration[6.0]
   def change
     create_table :aws_texts do |t|
       t.string :title
-      t.string  :content
+      t.text　:content
       t.timestamps
     end
   end
