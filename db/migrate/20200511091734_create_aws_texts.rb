a9863475742410eea65a56f1bf508daa8f8247fd
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-12 19:20:13 +0900

    バリデーションの追加

--- db/migrate/20200511091734_create_aws_texts.rb
+++ db/migrate/20200511091734_create_aws_texts.rb
@@ -1,8 +1,8 @@
 class CreateAwsTexts < ActiveRecord::Migration[6.0]
   def change
     create_table :aws_texts do |t|
-      t.string :title
-      t.text :content
+      t.string :title , nill: false
+      t.text :content , nill: false
       t.timestamps
     end
   end
--- db/migrate/20200511092548_create_questions.rb
+++ db/migrate/20200511092548_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-      t.string :title
-      t.text :detail
+      t.string :title, nill: false
+      t.text :detail, nill: false
       t.timestamps
     end
   end
