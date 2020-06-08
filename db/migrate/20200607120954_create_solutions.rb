98b5041faf7d278048a110eb792dbf4fc19ae784
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-06-08 21:06:30 +0900

    リンクの修正、showアクションの追加、questionとの関連づけ

--- db/migrate/20200607120954_create_solutions.rb
+++ db/migrate/20200607120954_create_solutions.rb
@@ -1,6 +1,7 @@
 class CreateSolutions < ActiveRecord::Migration[6.0]
   def change
     create_table :solutions do |t|
+      t.integer :question_id
       t.text :content
       t.timestamps
     end
