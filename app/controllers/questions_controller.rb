3d08533ed5062587bd3d6de162e5cd4946b17df3
yuki.t <cxntp@MacBook-Pro.local>
2020-06-11 19:01:34 +0900

    22質問投稿ページ実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,23 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice]="登録に成功しました"
+      redirect_to action: :index
+    else
+      @questions = Question.all
+      render action: :index
+    end
+  end
+
+  private
+  def question_params
+    params.permit(:title,:detail)
+  end
+
+
+
 end
